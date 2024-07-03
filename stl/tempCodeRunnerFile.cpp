790ac0e5e986d85388cb67dfda241aae7220f554
Kaushik <kaushikt422@gmail.com>
2024-07-03 10:00:26 +0530

    random

--- stl/tempCodeRunnerFile.cpp
+++ stl/tempCodeRunnerFile.cpp
@@ -1,22 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pairs(){
+void explainVector(){
 
-    pair<int ,int > a = {20,40};
-    cout<<a.first<<endl<<a.second<<endl;
+    // implement a vector and declare and insert element 
 
-    pair <int , pair <int ,int >> p ={20 ,{40,60} };
-    cout<<a.first << " " <<p.second.first <<" "<<p.second.second<<endl;
-    
-    pair<int , int > arr[] = {{1,2},{3,4},{5,6}};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<n;i++){
-        cout<<arr[i].first<< " "<<arr[i].second <<endl;
+    vector<int> v;
+    v.push_back(10);
+    v.emplace_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    v.push_back(60);
+    v.push_back(70);
+
+
+    vector<pair<int ,int>>vec;
+    vec.push_back({1,2});
+    vec.emplace_back(2,4);
+
+    vector <int> v1(5 , 100);
+    vector<int> v2(v);
+
+    // print the vector or itrate the vector
+
+    for(auto it = v2.begin(); it != v2.end();it++){
+        cout<<*(it)<<" ";
+        
+    }
+    cout<<endl;
+    for(auto it : v2){
+        cout<< it << " ";
+    }
+
+    // delete the element of the vector
+    v.erase(v.begin()+2,v.begin()+4);
+    for(auto it : v2){
+        cout<< it << " ";
     }
 }
 
 int main(){
-
-    pairs();
+    explainVector();
 }
