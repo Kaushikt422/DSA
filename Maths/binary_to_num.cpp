574ba80629c820c314f51775533c69fff66bf867
Kaushik tiwari <kaushikt422@gmail.com>
2025-07-31 20:01:53 +0530

    random

--- Maths/binary_to_num.cpp
+++ Maths/binary_to_num.cpp
@@ -1,21 +1,49 @@
 #include <bits/stdc++.h>
- using namespace std;
+using namespace std;
 
- int convert_binary_to_number(int num){
-    int sum=0;
+bool is_binary(int num)
+{
+    while (num > 0)
+    {
+        int digit = num % 10;
+
+        if (digit != 0 && digit != 1)
+        {
+            return false;
+        }
+        num = num / 10;
+    }
+    return true;
+}
+
+int convert_binary_to_number(int num)
+{
+    int sum = 0;
     int pow = 1;
-    while(num>0){
-        int rem = num % 10 ;
-        num = num / 10 ;
-        sum = sum + (rem*pow);
+    while (num > 0)
+    {
+        int rem = num % 10;
+        num = num / 10;
+        sum = sum + (rem * pow);
         pow = pow * 2;
     }
-    return sum ;
- }
+    return sum;
+}
 
-int main() {
+int main()
+{
     long long num;
-    cin>>num;
-    cout<< convert_binary_to_number(num)<<endl;
+    cin >> num;
+
+    if (is_binary(num) == true)
+    {
+
+        cout << convert_binary_to_number(num) << endl;
+    }
+    else
+    {
+        cout << "invalid number plz enter binary digits ";
+    }
+
     return 0;
 }
--- Maths/tempCodeRunnerFile.cpp
+++ Maths/tempCodeRunnerFile.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
  using namespace std;
 
+bool is_binary(int num ){
+    while(num>0){
+    int digit = num % 10 ;
+
+    if(digit != 0 && digit != 1 ){
+        return false;
+    }
+   num = num /10;
+}
+return true;
+}
+
  int convert_binary_to_number(int num){
     int sum=0;
     int pow = 1;
@@ -16,6 +28,15 @@
 int main() {
     long long num;
     cin>>num;
-    cout<< convert_binary_to_number(num)<<endl;
+
+    if(is_binary(num)==true){
+
+        cout<< convert_binary_to_number(num)<<endl;
+
+    }
+    else {
+        cout<<"invalid number plz enter binary digits ";
+    }
+    
     return 0;
 }
