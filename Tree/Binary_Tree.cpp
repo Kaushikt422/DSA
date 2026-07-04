83c199489f0683b0293c2764b728fb37a0102a6b
Kaushik tiwari <kaushikt422@gmail.com>
2026-07-05 01:23:14 +0530

    random

---
+++ Tree/Binary_Tree.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int indx =  -1;
+Node *BuildTree(vector<int> preorder){
+
+    indx++;
+    if(preorder[indx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[indx]);
+    root->left = BuildTree(preorder);
+    root->right = BuildTree(preorder);
+
+    return root;
+
+}
+// pre order traversall
+
+void preOrder(Node* root){
+
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data;
+    preOrder(root->left);
+    preOrder(root->right);
+};
+
+int main(){
+
+    vector<int> preorder = {2, 3, 5, -1, -1, 6, -1, -1, 4, 7, -1, -1, 8, -1, -1};
+    Node* root = BuildTree(preorder);
+    //for check the buildtree function and tree build or not 
+    // cout<< root->data<<endl;
+    // cout<< root->left->data<<endl;
+    // cout<< root->right->data;
+    preOrder(root); // preorder traversall
+    
+
+    return 0;
+}
